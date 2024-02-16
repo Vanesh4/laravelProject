904be6d5fbf20e067ab311238d85d364d2acf867
vanessa <nva.gomo926@gmail.com>
2024-02-16 16:30:46 -0500

    conexion a sql server

--- resources/views/asociados/index.blade.php
+++ resources/views/asociados/index.blade.php
@@ -1,32 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
     <h1>Lista de Asociados</h1>
-
-    
-    
-    <!-- Mostrar todos los asociados -->
-    <ul>
-        @foreach ($asociados as $asociado)
-        <div>
-            <p>Detalles del Asociado:</p>
-            <p>ID: {{ $asociado->cedula }}</p>
-            <p>Nombre: {{ $asociado->nombre }}</p>
-            <!-- Mostrar más detalles según sea necesario -->
-        </div>
-        @endforeach
-    </ul>
-    
     <form action="{{ route('asociados.show', ['asociado' => 'ID']) }}" method="GET">
-        <input type="text" name="id" placeholder="Buscar por ID">
+        <input type="text" name="id" placeholder="Buscar por cedula">
         <button type="submit">Buscar</button>
     </form>
-
+    @foreach ($asociados as $asociado)
+    <p>{{ $asociado->cedula }}</p>
+    @endforeach
+    <!-- Mostrar todos los asociados -->
+    <div class="mx-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Cédula</th>
+                    <th>Apellido</th>
+                    <th>Nombre</th>
+                    <th>Distrito ID</th>
+                    <th>Dirección</th>
+                    <th>Ciudad ID</th>
+                    <th>Estado</th>
+                    <th>Celular</th>
+                    <th>Email</th>
+                    <th>Observación Familia</th>
+                    <th>Observación</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($asociados as $asociado)
+                    <tr>
+                        <td>{{ $asociado->cod_cli }}</td>
+                        <td>{{ $asociado->apellido }}</td>
+                        <td>{{ $asociado->nombre }}</td>
+                        <td>{{ $asociado->cod_dis }}</td>
+                        <td>{{ $asociado->dir_casa }}</td>
+                        <td>{{ $asociado->cod_ciu }}</td>
+                        <td>{{ $asociado->estado }}</td>
+                        <td>{{ $asociado->tel_casa }}</td>
+                        <td>{{ $asociado->email }}</td>
+                        <td>{{ $asociado->benef }}</td>
+                        <td>{{ $asociado->observ }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
